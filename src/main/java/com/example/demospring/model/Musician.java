ffe2e659ad9ab0cff2ec4702d789ef642f333349
iquinto <isaganiquinto@gmail.com>
2021-01-30 18:11:06 +0100

    add code

---
+++ src/main/java/com/example/demospring/model/Musician.java
@@ -0,0 +1,47 @@
+package com.example.demospring.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class Musician {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+    
+    @NotNull
+    @Column(nullable = false)
+    private Date birth;
+
+    private Date death;
+
+    @NotNull
+    @Column(nullable = false)
+    private String gender;
+
+
+    @NotNull
+    @Column(nullable = false)
+    private String nationality;
+
+    public Musician() {
+    }
+
+    public Musician(@NotNull String name, @NotNull Date birth, @NotNull String gender, @NotNull String nationality) {
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+        this.nationality = nationality;
+    }
+}
